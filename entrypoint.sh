195cf1f08208d0b2980e1199c1000e674116bc00
Tudor <7089284+tudddorrr@users.noreply.github.com>
2022-04-11 20:12:42 +0100

    checkout TARGET_BRANCH and create if it doesnt exist

--- entrypoint.sh
+++ entrypoint.sh
@@ -74,24 +74,17 @@ git config --global user.name "$USER_NAME"
 git config --global http.version HTTP/1.1
 
 {
-	git clone --single-branch --depth 1 --branch "$TARGET_BRANCH" "$GIT_CMD_REPOSITORY" "$CLONE_DIR"
-} || {
-    if [ "$CREATE_TARGET_BRANCH_IF_NEEDED" = "true" ]
-    then
-        # Default branch of the repository is cloned. Later on the required branch
-	# will be created
-        git clone --single-branch --depth 1 "$GIT_CMD_REPOSITORY" "$CLONE_DIR"
-    else
-        false
-    fi
+	git clone --single-branch "https://$USER_NAME:$API_TOKEN_GITHUB@$GITHUB_SERVER/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git" "$CLONE_DIR"
 } || {
 	echo "::error::Could not clone the destination repository. Command:"
-	echo "::error::git clone --single-branch --branch $TARGET_BRANCH $GIT_CMD_REPOSITORY $CLONE_DIR"
-	echo "::error::(Note that if they exist USER_NAME and API_TOKEN is redacted by GitHub)"
-	echo "::error::Please verify that the target repository exist AND that it contains the destination branch name, and is accesible by the API_TOKEN_GITHUB OR SSH_DEPLOY_KEY"
+	echo "::error::git clone --single-branch https://$USER_NAME:the_api_token@$GITHUB_SERVER/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git $CLONE_DIR"
+	echo "::error::(Note that the USER_NAME and API_TOKEN is redacted by GitHub)"
+	echo "::error::Please verify that the target repository exist AND that it contains the destination branch name, and is accesible by the API_TOKEN_GITHUB"
 	exit 1
-
 }
+
+git checkout -B "$TARGET_BRANCH"
+
 ls -la "$CLONE_DIR"
 
 TEMP_DIR=$(mktemp -d)
